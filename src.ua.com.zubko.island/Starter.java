1df630cff0b95f106a4d7cccdaab72117b2c0432
Hryhorii Zubko <zubko.hryhorii@gmail.com>
2022-12-30 14:26:40 +0200

    deleted comments

--- src.ua.com.zubko.island/Starter.java
+++ src.ua.com.zubko.island/Starter.java
@@ -5,8 +5,7 @@ import service.LifeSimulator;
 public class Starter {
 
     public static void main(String[] args) {
-//         SettingReader sr = new SettingsReader();
-//         sr.getSettings // return instance of WorldSettings class
+
         Island island = new Island();
         new LifeSimulator(island).runLocations();
 
